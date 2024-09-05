ccf44828f39de5b53938a54c116c46171ecb3c39
Joyboy <kudamono200659@gmail.com>
2024-09-04 22:30:05 -0500

    terminamos otra clases

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { TitleComponent } from '../../../shared/title/title.component';
 
 @Component({
   standalone: true,
-  imports: [],
-  templateUrl: './change-detection.component.html',
-  styles: ``
+  imports: [CommonModule, TitleComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+    <app-title [title]="currentFramework()"></app-title>
+
+    <pre>{{frameworksAsSignal() | json }} </pre>
+    <pre>{{frameworksAsProperty | json }} </pre>
+  `
 })
 export default class ChangeDetectionComponent {
 
+  public currentFramework = computed(
+    () => `Change detection - ${  this.frameworksAsSignal().name}`
+  )
+
+  public frameworksAsSignal = signal({
+    name:'Angular',
+    release: 2016
+  })
+
+  public frameworksAsProperty = {
+    name:'Angular',
+    release: 2016
+  }
+
+  constructor(){
+
+    setTimeout(()=>{
+      // this.frameworksAsProperty.name = 'React'
+      this.frameworksAsSignal.update(value => ({
+        ...value,
+        name: "React"
+      }))
+    }, 3000 )
+  }
+
+  
 }
