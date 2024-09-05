6a8388de51acdefdc530e05e74379f3209734313
Joyboy <kudamono200659@gmail.com>
2024-09-04 22:52:44 -0500

    defer

--- src/app/dashboard/pages/defer-views/defer-views.component.ts
+++ src/app/dashboard/pages/defer-views/defer-views.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { AppComponent } from "../../../app.component";
+import { HeavyLoadersSlowComponent } from "../../../shared/heavy-loaders/heavy-loaders-slow.component";
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [AppComponent, HeavyLoadersSlowComponent],
   templateUrl: './defer-views.component.html',
   styles: ``
 })
--- src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
+++ src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
@@ -1,14 +1,24 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-heavy-loaders-slow',
   standalone: true,
   imports: [CommonModule],
   template: `
-    <h1>Hola Mundo</h1>
+    <section [ngClass]="['w-full h-[600px]', cssClass]">
+
+    </section>
   `
 })
 export class HeavyLoadersSlowComponent {
+
+  @Input({required:true}) cssClass!: string;
+
+  constructor(){
+    const start = Date.now();
+    while(Date.now() - start < 3000){}
+    console.log("cargado")
+  }  
 
 }
