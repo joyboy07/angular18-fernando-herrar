9ec0264b7cad2d7f3f98923741cfdcc80a34f5e0
Joyboy <kudamono200659@gmail.com>
2024-09-04 21:16:23 -0500

    Inicioamos el proyecto

---
+++ src/app/dashboard/pages/view-transitions/view-transitions.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './view-transitions.component.html',
+  styles: ``
+})
+export default class ViewTransitionsComponent {
+
+}
---
+++ src/app/shared/title/title.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-title',
+  standalone: true,
+  imports: [],
+  templateUrl: './title.component.html',
+})
+export class TitleComponent {
+
+}
